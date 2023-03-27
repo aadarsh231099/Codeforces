ed6132c68ea51dce708d5359066a4e8a4d1ff5c2
Aadarsh <aadarshhanu23@gmail.com>
2023-03-27 12:50:03 +0530

    cf

---
+++ 630A-Again Twenty Five!.cpp
@@ -0,0 +1,21 @@
+//https://codeforces.com/problemset/problem/630/A
+//Again Twenty Five!
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  long long int n;
+  cin>>n;
+  if(n>=2)
+  cout << "25" << endl;
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t=1;
+  //cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
