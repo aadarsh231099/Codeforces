f45d8193b281f76866e6ccf818c42234635d19e4
Aadarsh <aadarshhanu23@gmail.com>
2023-04-03 17:08:48 +0530

    cf

---
+++ 1370A-Maximum GCD.cpp
@@ -0,0 +1,22 @@
+//https://codeforces.com/problemset/problem/1370/A
+//Maximum GCD
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+  long long n;
+  cin>>n;
+  cout<<n/2<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
+
