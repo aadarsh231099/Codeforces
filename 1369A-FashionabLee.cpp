308145617bb9caa5ea68c0bab41c048c2bdeba50
Aadarsh <aadarshhanu23@gmail.com>
2023-04-26 07:20:35 +0530

    cf

---
+++ 1369A-FashionabLee.cpp
@@ -0,0 +1,29 @@
+//https://codeforces.com/problemset/problem/1369/A
+//FashionabLee
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+        if(n%4==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  long long t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
+
