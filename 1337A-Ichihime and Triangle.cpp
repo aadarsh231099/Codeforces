0adc27318670c3314135ffb0f661c7b677113c90
Aadarsh <aadarshhanu23@gmail.com>
2023-04-27 22:26:29 +0530

    cf

---
+++ 1337A-Ichihime and Triangle.cpp
@@ -0,0 +1,24 @@
+//https://codeforces.com/problemset/problem/1337/A
+//Ichihime and Triangle
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+void solve(){
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  long long t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
+
