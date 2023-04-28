7f7b243fdbebbca6af669bd07ab4874b2018ab81
Aadarsh <aadarshhanu23@gmail.com>
2023-04-28 23:30:48 +0530

    cf

---
+++ 1372A-Omkar and Completion.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/problemset/problem/1372/A
+//Omkar and Completion
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+void solve(){
+        int n;
+        cin>>n;
+        for(int i=0; i<n; i++){
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  long long t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
+
